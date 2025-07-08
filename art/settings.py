0fe0e72b5c045b865dae5d2c814bd7c19db52d19
TechAce <93094408+gauranshivarshney@users.noreply.github.com>
2025-07-08 20:21:03 +0530

    final

--- art/settings.py
+++ art/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9^w_$m5!*0y^mxftqod=e-osa0qbg^01)a_$37*m&j8rn_)ggi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
